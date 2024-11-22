bca002cbc7c241249eb54b55bed0b373eb7ee7d2
Chinmay <155734661+iiichinmay@users.noreply.github.com>
2024-11-22 11:01:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+// import LoginPage from './components/login/login.jsx';
+// import  TodoApp  from './components/ToDo/index.jsx';
+// import  from './components/useState/index.jsx';
+// import RandomColor from './components/randomColors/index.jsx';
+// import Greet from './components/Greeting/index.jsx';
+// import Counter from './components/Counter/index.jsx';
+// import WeatherApp from './components/hooks/index.jsx';
+import Accordian from './components/Accordian/accordian.jsx';
+
+
+function App() {
+  return (
+    <div>
+
+      {/* <LoginPage /> */}
+      {/* <ThemeToggle /> */}
+      {/* <ItemList /> */}
+      {/* <TodoApp /> */}
+      {/* <RandomColor /> */}
+      {/* <Greet /> */}
+      {/* <Counter /> */}
+      {/* <WeatherApp /> */}
+      <Accordian />
+
+    </div>
+  );
+}
+
+export default App;
+
